bc671fa77b6a10c53fe9b8ee77b4f30b02702531
xgengine <1403450840@qq.com>
2018-10-31 22:24:21 +0800

    去掉下载的插件

--- Assets/H3D.CResources/Editor/Script/BundleBuilder/BundleBuilder.cs
+++ Assets/H3D.CResources/Editor/Script/BundleBuilder/BundleBuilder.cs
@@ -29,7 +29,7 @@ namespace H3D.EditorCResources
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             DepBundleBuilder builder = property.serializedObject.targetObject as DepBundleBuilder;
-            builder.m_Options = (BuildAssetBundleOptions)EditorGUI.EnumFlagsField(position, "Options", builder.m_Options);
+            builder.m_Options = (BuildAssetBundleOptions)EditorGUI.EnumMaskField(position, "Options", builder.m_Options);
         }
     }
 }
